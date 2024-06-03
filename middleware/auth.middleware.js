01d825ee3a0f22fa065a50bb3cc0c79599ec3a66
itsme-harsh <harshdevadiya.ce20@sltiet.edu.in>
2024-06-03 12:12:54 +0530

    changed git

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -5,20 +5,15 @@ import { asyncHandler } from "../utils/ayncHandler.js";
 
 export const verifyJWT = asyncHandler(async (req, _, next) => {
     try {
-        console.log(process.env.ACCESS_TOKEN_SECRET)
         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
 
-        console.log(token)
         if (!token) {
             throw new ApiError(401, "Unauthorised request");
         }
 
-
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-        console.log("decodedToken: ",decodedToken);
 
         const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
-        console.log("user: ",user);
 
         if (!user) {
             throw new ApiError(401, "Invalid Access Token");
